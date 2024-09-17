69c2f58b223f8630d2deafcb8b1fb74c4afbfd0f
Adam DeJans Jr. <adam.dejans@gmail.com>
2024-09-16 20:06:41 -0400

    Adding placeholders and setting up React (#12)
    * Adding placeholders and setting up React
    
    ---------
    
    Co-authored-by: Blair Sibu <blairsibu@gmail.com>

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,20 +1,38 @@
 import React, { useState } from 'react';
-//test
+import { useEffect } from 'react';
+import type { MapNodeProps } from './MapNode';
+import MapNode from './MapNode'
+
 const Home = () => {
+  const [startPoint, setStartPoint] = useState<MapNodeProps>({
+    latitude: 0,
+    longitude: 0,
+  });
+  const [endPoint, setEndPoint] = useState<MapNodeProps>({
+    latitude: 0,
+    longitude: 0,
+  });
+  const [carPoint, setCarPoint] = useState<MapNodeProps>({
+    latitude: 0,
+    longitude: 0,
+  });
 
-    [startPoint, setStartPoint] = useState();
-    [endPoint, setStartPoint] = useState();
-    [carPoint, setCarPoint] = useState();
+  useEffect(() => {
+    setStartPoint(startPoint);
+    setEndPoint(endPoint);
+    setCarPoint(carPoint);
+  }, [startPoint, endPoint, carPoint]);
 
-    return (
+  return (
     <>
-        <Map
-            startPoint={startPoint}
-            endPoint={endPoint}
-            carPoint={carPoint}
-         />
+      <header>
+        <h1>This is a map placeholder.</h1>
+        <p>Start Location: <MapNode latitude={startPoint.latitude} longitude={startPoint.longitude}/></p>
+        <p>End Location: <MapNode latitude={endPoint.latitude} longitude={endPoint.longitude}/></p>
+        <p>Car Location: <MapNode latitude={carPoint.latitude} longitude={carPoint.longitude}/></p>
+      </header>
     </>
-    )
+  );
 };
 
 export default Home;
---
+++ frontend/src/components/MapNode.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export interface MapNodeProps {
+  latitude: number;
+  longitude: number;
+}
+
+const MapNode = ({ latitude, longitude }: MapNodeProps) => {
+  return <div> <blockquote>Lat = {latitude}; Lon = {longitude} </blockquote></div>;
+};
+
+export default MapNode;
